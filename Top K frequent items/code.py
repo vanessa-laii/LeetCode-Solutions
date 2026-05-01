37edb0780cad5fc9019bd3cd883930c33258825f
vanessa-laii <vanessa.sw.lai@gmail.com>
2026-05-01 15:09:43 -0400

    leetcode questions

--- Top K frequent items/code.py
+++ Top K frequent items/code.py
@@ -17,6 +17,26 @@ class Solution:
         
         
 
+#version 2 - optimized
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # count the number of occurences for each
+        count = collections.Counter(nums)
+        heap = []
+        # build a maxheap for all the occurences 
+        for key, value in count.items():
+            heapq.heappush(heap, (value, key))
+            # the max len of heap cannot exceed k
+            if len(heap) > k:
+                heapq.heappop(heap)
+        # for every pair in the heap, return the key
+        return [num for value, num in heap]
+        
+        
 
+
+
+        
 
         
