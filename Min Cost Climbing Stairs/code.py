a7511e6962d394bc8b4deb3e1b78d34b9976599c
vanessa-laii <vanessa.sw.lai@gmail.com>
2026-05-21 22:27:29 -0400

    update schemas link

---
+++ Min Cost Climbing Stairs/code.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        length = len(cost)
+        dp = {}
+        #returns cost
+        def recur(index):
+            if index in dp:
+                return dp[index]
+            # because there will be no more cost past index
+            if index >= length:
+                return 0 
+
+            one = recur(index + 1) + cost[index]
+            two = recur(index + 2) + cost[index]
+            dp[index] = min(one, two)
+            return dp[index]
+        
+        return min(recur(0), recur(1))
+            
+
+        
--- Validate BST/code.py
+++ Validate BST/code.py
@@ -25,3 +25,30 @@ class Solution:
             if not result[i] > result[i-1]:
                 return False
         return True
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        def bfs(node, lowerBound, upperBound):
+            if not node:
+                return True
+
+            if not lowerBound < node.val < upperBound:
+                return False
+            
+            return bfs(node.left, lowerBound, node.val) and bfs(node.right, node.val, upperBound)
+        
+        return bfs(root, float("-inf"), float("inf"))
+            
+            
+
+        
